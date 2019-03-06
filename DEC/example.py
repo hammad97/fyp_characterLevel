a87fc456cdabc65e8ea193b8dcf57dc79ad676ee
moizzaveri <42716365+moizzaveri@users.noreply.github.com>
2019-03-06 23:13:07 +0500

    Add files via upload

---
+++ DEC/example.py
@@ -0,0 +1,30 @@
+from keras_dec import DeepEmbeddingClustering
+from keras.datasets import mnist
+import numpy as np
+
+
+def get_mnist():
+    np.random.seed(1234) # set seed for deterministic ordering
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_all = np.concatenate((x_train, x_test), axis = 0)
+    Y = np.concatenate((y_train, y_test), axis = 0)
+    X = x_all.reshape(-1,x_all.shape[1]*x_all.shape[2])
+    
+    p = np.random.permutation(X.shape[0])
+    X = X[p].astype(np.float32)*0.02
+    Y = Y[p]
+    return X, Y
+
+
+X, Y  = get_mnist()
+
+#from numpy import genfromtxt
+#X = genfromtxt('experiment_100_dense3.csv', delimiter=',')
+
+c = DeepEmbeddingClustering(n_clusters=10, input_dim=784)
+c.initialize(X, finetune_iters=100000, layerwise_pretrain_iters=50000)
+c.cluster(X, y=Y)
+
+#c = DeepEmbeddingClustering(n_clusters=20, input_dim=20)
+#c.initialize(X, finetune_iters=100000, layerwise_pretrain_iters=50000)
+#c.cluster(X, save_interval=50000)
